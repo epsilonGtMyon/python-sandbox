a6abe206107aa6fe9aececa8c16caf7f5eaa2e09
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2024-10-06 13:06:22 +0900

    middlewareを追加

---
+++ fastapi-sandbox/app/common/middleware/process_time_middleware.py
@@ -0,0 +1,15 @@
+import logging
+import time
+from fastapi import Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
+
+logger = logging.getLogger(__name__)
+
+
+class ProcesstimeMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next) -> Response:
+        start_time = time.perf_counter()
+        response = await call_next(request)
+        process_time = time.perf_counter() - start_time
+        logger.info("%s %s", request.url, process_time)
+        return response
--- fastapi-sandbox/main.py
+++ fastapi-sandbox/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from app.common.middleware.logging_middleware import LoggingMiddleware
+from app.common.middleware.process_time_middleware import ProcesstimeMiddleware
 from app.endpoint.sandbox01 import sandbox01
 from app.endpoint.sandbox02 import sandbox02
 from app.endpoint.sandbox03 import sandbox03
 
 app = FastAPI()
+
+# ミドルウェア
+app.add_middleware(LoggingMiddleware)
+app.add_middleware(ProcesstimeMiddleware)
 
 # 各ルートを追加
 app.include_router(sandbox01.router)
